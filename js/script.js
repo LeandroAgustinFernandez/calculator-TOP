e8893283ae01b97223eb429d26bc85450a43dd70
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2022-04-13 19:35:51 -0300

    Create function to control Mathematics Operators

--- js/script.js
+++ js/script.js
@@ -1,20 +1,47 @@
-function add(number1,number2){
-    let number1 = Number(number1);
-    let number2 = Number(number2);
-    return number1 + number2;
+let number1;
+let number2;
+let operation = null;
+
+function operator(operation, firstNumb, secondNumb) {
+  let result;
+  switch (operation) {
+    case "+":
+      result = add(firstNumb, secondNumb);
+      break;
+    case "-":
+      result = subtract(firstNumb, secondNumb);
+      break;
+    case "*":
+      result = multiply(firstNumb, secondNumb);
+      break;
+    case "/":
+      result = divide(firstNumb, secondNumb);
+      break;
+    default:
+      break;
+  }
+  return result;
 }
-function subtract(number1,number2){
-    let number1 = Number(number1);
-    let number2 = Number(number2);
-    return number1 - number2;
+
+function add(number1, number2) {
+  let num1 = Number(number1);
+  let num2 = Number(number2);
+  let res = num1 + num2;
+  return res;
 }
-function multiply(number1,number2){
-    let number1 = Number(number1);
-    let number2 = Number(number2);
-    return number1 * number2;
+function subtract(number1, number2) {
+  let num1 = Number(number1);
+  let num2 = Number(number2);
+  return num1 - num2;
 }
-function divide(number1,number2){
-    let number1 = Number(number1);
-    let number2 = Number(number2);
-    return (number2 > 0) ? number1/number2 : 'ERROR';
+function multiply(number1, number2) {
+  let num1 = Number(number1);
+  let num2 = Number(number2);
+  return num1 * num2;
 }
+function divide(number1, number2) {
+  let num1 = Number(number1);
+  let num2 = Number(number2);
+  return number2 > 0 ? num1 / num2 : "ERROR";
+}
+
